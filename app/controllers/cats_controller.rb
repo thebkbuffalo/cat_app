ad6e23a7602e6b22a2c8a41b2d13eb0022abec4e
Evan Berg <evan.m.berg@gmail.com>
2021-08-31 20:23:12 -0400

    updaed cors, gems, cat model to get url easily, routes, and made a quick little front end for testing

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,8 +1,8 @@
 class CatsController < ApplicationController
   include ActionController::MimeResponds
   def index
-    @cats = Cat.all.order(created_at: :desc)
-    format.json {render json: {status: 200, cats: @cats}}
+    @cats = Cat.all.order(created_at: :desc).map{|c| {id: c.id, created_at: c.created_at.strftime('%d/%m/%y'), url: c.get_image_url}}
+    render json: {status: 200, cats: @cats}
   end
   
 
@@ -12,7 +12,7 @@ class CatsController < ApplicationController
   end
 
   def create
-    cat = Cat.new(cat_params)
+    cat = Cat.new(image: cat_params)
     if cat.save
       render json: {status: 200, message: "cat img saved"}
     else
@@ -25,18 +25,13 @@ class CatsController < ApplicationController
 
   end
 
-  def delete
-    cat = Cat.find(params[:id])
-    if cat.destroy
-      render json: {status: 200, message: "cat img deleted"}
-    else
-      # figure out how to get status code
-      render json: {status: 400, message: "something went wrong"}
-    end
+  def destroy
+    binding.pry
+    
   end
 
   private
     def cat_params
-      params.require(:cat).permit(:image)
+      params.permit(:image).require(:image)
     end
 end
--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,5 +1,10 @@
 class Cat < ApplicationRecord
   has_one_attached :image
+  include Rails.application.routes.url_helpers
 
   validates :image, { presence: true }
+
+  def get_image_url
+    url_for(self.image)
+  end
 end
